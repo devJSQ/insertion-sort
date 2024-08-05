a9a9fa8442cfe1f3009557ea1ed34d8207b46fd1
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-08-05 19:14:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
 public class Main {
+  public static char [] InsertionSort(char [] letters){
+    for(int i = 1; i < letters.length; i++){
+      char val = letters[i];
+      int j = i - 1;
+      while (j >=0 && letters[j] > val){
+        letters[j + 1] = letters[j];
+        j--;
+      }
+      letters[j + 1] = val;
+      
+    }
+    
+    return letters;
+    
+  }
 
   public static void main(String[] args){
+    char [] letters = {'e', 'b', 'd', 'a', 'c'};
+    System.out.println(letters);
+    InsertionSort(letters);
+    System.out.println(letters);
 
-  /* your code here */
+
 	
   }
 }
